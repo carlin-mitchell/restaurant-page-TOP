ede793dee6a6ab8cd88fb4337dd7bbd1a5b01d29
Carlin Mitchell <carlinmi@umn.edu>
2023-05-31 14:01:08 -0500

    “fix nav button animation triggering”

--- src/components/NavButton.js
+++ src/components/NavButton.js
@@ -4,7 +4,7 @@ const NavButton = (buttonID) => {
   const button = Object.assign(document.createElement("div"), {
     id: `toggle-view-${buttonID}`,
     innerText: `View ${buttonID}`,
-    classList: "nav-button un-selectable-text ",
+    classList: "nav-button un-selectable-text",
     onclick: function () {
       screen.toggleView(this.id);
     },
--- src/logic/ScreenController.js
+++ src/logic/ScreenController.js
@@ -5,6 +5,11 @@ const ScreenController = (() => {
     //hide all views
     allViews.forEach((view) => view.classList.add("hidden"));
 
+    const allNavButtons = document.querySelectorAll(".nav-button");
+    const clickedNavButton = document.querySelector(`#${buttonID}`);
+    allNavButtons.forEach((button) => (button.style.animation = ""));
+
+    clickedNavButton.style.animation = "nav-button-animation 1s forwards";
     //display the target view
     const view = document.querySelector("#" + viewID);
     view.classList.remove("hidden");
