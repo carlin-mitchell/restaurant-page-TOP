ff8ebc86862b61e74b9ffdc3945b29817bb14505
Carlin Mitchell <carlinmi@umn.edu>
2023-06-02 09:12:32 -0500

    “sync”

--- src/components/View.js
+++ src/components/View.js
@@ -2,7 +2,6 @@ const View = (viewID) => {
   const view = Object.assign(document.createElement("div"), {
     id: `view-${viewID}`,
     className: `${viewID} view hidden`,
-    innerText: `view-${viewID}`,
   });
 
   return view;
