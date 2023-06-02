bf21a2f007ed6444a1e406cd6ef9849ce938b58f
Carlin Mitchell <carlinmi@umn.edu>
2023-06-02 09:05:07 -0500

    “change view width and add animation”

--- src/logic/ScreenController.js
+++ src/logic/ScreenController.js
@@ -1,18 +1,26 @@
 const ScreenController = (() => {
   const toggleView = (buttonID) => {
-    const viewID = `view-${buttonID.split("-").slice(-1)}`;
-    const allViews = document.querySelectorAll(".view");
-    //hide all views
-    allViews.forEach((view) => view.classList.add("hidden"));
+    const animationTransitionTimeSeconds = (1).toString() + "s";
+    const viewId = `view-${buttonID.split("-").slice(-1)}`;
+    const viewIdIdentifier = viewId.split("-")[1];
 
+    //hide all views
+    const allViews = document.querySelectorAll(".view");
+    allViews.forEach((view) => {
+      view.classList.add("hidden");
+      view.style.animation = "";
+    });
+    //display the target view
+    const view = document.querySelector("#" + viewId);
+    view.classList.remove("hidden");
+    view.style.animation = `view-animation-${viewIdIdentifier} ${animationTransitionTimeSeconds} forwards`;
+
+    //reset all nav button backgrounds
     const allNavButtons = document.querySelectorAll(".nav-button");
     const clickedNavButton = document.querySelector(`#${buttonID}`);
+    //animate the clicked nav button bg color to the corresponding view bg color
     allNavButtons.forEach((button) => (button.style.animation = ""));
-
-    clickedNavButton.style.animation = "nav-button-animation .2s forwards";
-    //display the target view
-    const view = document.querySelector("#" + viewID);
-    view.classList.remove("hidden");
+    clickedNavButton.style.animation = `nav-button-animation-${viewIdIdentifier} ${animationTransitionTimeSeconds} forwards`;
   };
   return { toggleView };
 })();
